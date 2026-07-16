37eeb5e0472a2e7c02c0c6841081aeaa57e754d1
Konoplev Aleksandr <konoplev.a.a0000@gmail.com>
2026-07-17 04:28:52 +1000

    add custom user, command CSU

--- README.md
+++ README.md
@@ -6,6 +6,14 @@
 - Django Rest Framework
 - PostgreSQL
 ---
+### Команды
+`python manage.py create_custom_superuser` - Команда для создания суперпользователя
+
+---
+
+### Пользователь
+
+---
 
 ### Эндпоинты:
 
---
+++ users/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    """Модель пользователя"""
+
+    username = None
+    email = models.EmailField(max_length=255, unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
