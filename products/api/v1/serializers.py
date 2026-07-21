bb74614705243ae397330039051a8999bbc580bf
Konoplev Aleksandr <konoplev.a.a0000@gmail.com>
2026-07-21 18:49:03 +1000

    add end-point api/v1/category/list-with-subcategories/

--- products/api/v1/serializers.py
+++ products/api/v1/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework.fields import SerializerMethodField
-
+from products.api.v1.paginators import NestedSubcategoryPagination
 from products.models import Category, Subcategory, Product
 from rest_framework.serializers import ModelSerializer, SlugRelatedField
 
@@ -52,3 +52,30 @@ class CategorySerializer(ModelSerializer):
         model = Category
         fields = ['name', 'slug', 'image']
         read_only_fields = ['slug']
+
+
+class CategoryWithSubcategoriesSerializer(ModelSerializer):
+    """Список категория с пагинированным списком подкатегорий"""
+    subcategories = SerializerMethodField()
+
+    class Meta:
+        model = Category
+        fields = ['name', 'slug', 'image', 'subcategories']
+
+    def get_subcategories(self, obj):
+        request = self.context.get('request')
+        paginator = NestedSubcategoryPagination()
+        paginator.page_size = int(request.query_params.get('sub_page_size', 5))
+
+        sub_qs = obj.subcategories.all()
+        page = paginator.paginate_queryset(sub_qs, request)
+        if page is not None:
+            serializer = SubcategorySerializer(page, many=True, context=self.context)
+            return dict([
+                ('count', paginator.page.paginator.count),
+                ('page_size', paginator.page_size),
+                ('page', paginator.page.number),
+                ('results', serializer.data),
+            ])
+        serializer = SubcategorySerializer(sub_qs, many=True, context=self.context)
+        return serializer.data
